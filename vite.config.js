607e850ae973734bd589a057e2847cf69065ef2a
Joseph Becker <jbecker@gidindustrial.com>
2026-02-08 11:04:43 -0600

    Add Supabase env vars to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_MARKETCHECK_API_KEY': JSON.stringify(process.env.VITE_MARKETCHECK_API_KEY),
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    // Expose env variables to the client
+    define: {
+      __MARKETCHECK_API_KEY__: JSON.stringify(env.VITE_MARKETCHECK_API_KEY),
+      __SUPABASE_URL__: JSON.stringify(env.VITE_SUPABASE_URL),
+      __SUPABASE_ANON_KEY__: JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    },
   }
 })
